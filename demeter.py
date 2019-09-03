388a9608bf0d94a30809d9f271b04661c2f4a66b
Nico Gonzalez <nicholas.gonzalez@fsinvestments.com>
2019-09-03 16:42:24 -0400

    Modify script to return list of all tickets slated for deployment

--- demeter.py
+++ demeter.py
@@ -2,9 +2,10 @@ from github import Github
 import env
 
 git = Github(env.ACCESS_TOKEN)
-user = git.get_user()
 repo = git.get_repo(env.REPO)
+deploy_label = repo.get_label("Ready to Deploy")
 
-open_issues = repo.get_issues(state='open')
-for issue in open_issues:
-    print(issue)
+ready_to_deploy = repo.get_issues(state='open', sort='created', direction='desc', labels=[deploy_label])
+for ticket in ready_to_deploy:
+    print(ticket)
+
