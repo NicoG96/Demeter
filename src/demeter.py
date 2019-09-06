9897966903eaee667e2be763e1909267e82c620d
Nico Gonzalez <nicholas.gonzalez@fsinvestments.com>
2019-09-06 11:14:42 -0400

    Make cli speek english gooder

--- src/demeter.py
+++ src/demeter.py
@@ -18,16 +18,27 @@ def demeter_cli():
     print(colored('============================================', 'cyan'))
 
     pull_requests = None
+    connect_errors = None
     tickets = get_tickets()
 
     if len(tickets) is not 0:
-        pull_requests = get_pulls(tickets)
+        pull_requests, connect_errors = get_pulls(tickets)
     else:
         logging.error('No tickets were entered! Exiting...')
         exit(1)
 
     if len(pull_requests) is not 0:
-        pull_requests = sort_pulls(pull_requests)
+        if connect_errors:
+            print(colored('There was an error connecting ' + str(connect_errors) +
+                          ' ticket' +
+                          ('s' if connect_errors > 1 else '') +
+                          '. Would you still like to continue with deployment? [y/n]', 'yellow'))
+
+            if input() == 'y':
+                pull_requests = sort_pulls(pull_requests)
+            else:
+                logging.info('Exiting...')
+                exit(1)
     else:
         logging.error('Couldn\'t retrieve any associated pull requests. Exiting...')
         exit(1)
@@ -59,9 +70,11 @@ def get_tickets():
 
 
 def get_pulls(tickets):
-    logging.info('Connecting ' + str(len(tickets)) + ' issues to relevant pull requests...')
+    logging.info('Connecting ' + str(len(tickets)) + ' issue' +
+                 ('s' if len(tickets) > 1 else '') + ' to relevant pull requests...')
     all_pulls = repo.get_pulls(state = 'closed', sort = 'created', direction = 'desc')[:50]
     connected_pulls = []
+    errors = 0
 
     for ticket in tickets:
         match = False
@@ -73,10 +86,16 @@ def get_pulls(tickets):
                 break
 
         if match is False:
+            errors += 1
             logging.error('Did not find a connected PR for ticket #' + str(ticket)
                           + '.\nDid the PR include the ticket # in the title?')
 
-    return connected_pulls
+    logging.info('Connected ' + str(len(tickets) - errors) + ' issue' +
+                 ('s' if len(tickets)-errors > 1 else '') +
+                 ' to ' + str(len(connected_pulls)) +
+                 ' pull request' + ('s' if len(connected_pulls) > 1 else ''))
+
+    return connected_pulls, errors
 
 
 def sort_pulls(pull_requests):
