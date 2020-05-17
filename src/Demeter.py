d15d31d434693d0d4e5b0cd4df40a7309b6b77f4
Nico Gonzalez <NicoG96@gmail.com>
2020-05-17 02:15:53 -0400

    Add executor

---
+++ src/Demeter.py
@@ -0,0 +1,19 @@
+from BitBucket import BitBucket as bb
+from GitHub import GitHub as gh
+import argparse
+import logging
+
+
+if __name__ == "__main__":
+    logging.getLogger().setLevel(logging.INFO)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--service", "-s", help="Specify \"github\" or \"bitbucket\"", required=True)
+    parser.add_argument("--version", "-v", action="version", version="1.1.1")
+    args = parser.parse_args()
+
+    if args.service.lower() == "github":
+        gh().execute()
+    elif args.service.lower() == "bitbucket":
+        bb().execute()
+    else:
+        logging.error("Unknown service. Please specify either github or bitbucket")
