78e5cbe702e251e84d0c10b85b5f1644a6fc4a0e
Nico Gonzalez <NicoG96@gmail.com>
2020-05-29 21:13:44 -0400

    Add check for updates, semver dependency

--- src/Demeter.py
+++ src/Demeter.py
@@ -1,20 +1,29 @@
 from BitBucket import BitBucket as bb
 from GitHub import GitHub as gh
+from termcolor import colored
 import argparse
-import logging
+import requests
+import semver
+VERSION = "2.1.0"
+
+
+def check_for_updates():
+    releases = requests.get("https://api.github.com/repos/NicoG96/demeter/releases").json()
+    releases = sorted(releases, key=lambda x: x["published_at"], reverse=True)
+    formatted_version = str(releases[0]["tag_name"])[1:]
+
+    if semver.compare(formatted_version, VERSION) == 1:
+        print(colored(f"A new version of Demeter is available. Please consider upgrading to v{formatted_version}", "yellow"))
+    return
 
 
 if __name__ == "__main__":
-    logging.getLogger().setLevel(logging.INFO)
     parser = argparse.ArgumentParser()
     parser.add_argument("--github", "-g", action="store_true")
     parser.add_argument("--bitbucket", "-b", action="store_true")
-    parser.add_argument("--version", "-v", action="version", version="2.1.0")
+    parser.add_argument("--version", "-v", action="version", version=VERSION)
     args = parser.parse_args()
 
-    if args.github == True:
-        gh().execute()
-    elif args.bitbucket == True:
-        bb().execute()
-    else:
-        logging.error("Please specify which service you are using by passing a --github/-g or --bitbucket/-b flag when invoking Demeter.")
+    if args.github or args.bitbucket:
+        check_for_updates()
+        gh().execute() if args.github else bb().execute()
